6bb1f6178d33dc181412a7408c61a5d8db9345d5
Steve Hussung <shussung@indot.in.gov>
2024-05-16 17:11:19 -0400

    Added Hand class

--- Main.ps1
+++ Main.ps1
@@ -31,8 +31,65 @@ class Card
   }
 }
 
-$myCard = [Card]::new("3", "H")
+class Hand
+{
+    [Card[]] $Cards
+
+    # Empty hand for default initialization
+    Card() {$this.Init(@())}
+
+    Hand([Card[]] $importHand)
+    {
+        $this.Init($importHand)
+    }
+
+    [void] Init([Card[]] $importHand)
+    {
+        $this.Cards = $importHand
+    }
+    [string] ToString() 
+    {
+        $arrayOfStrings = 
+        $this.Cards | 
+        ForEach-Object {
+            [string]$_
+        }
+        return $arrayOfStrings -join "`n"
+    }
+    [int] AceCount()
+    {
+        return ($this.Cards | Where-Object -Property Rank -eq 'A' | Measure-Object).Count
+    }
+    [int] Points()
+    {
+        $maxPoints = ($this.Cards | ForEach-Object {$_.Points()} | Measure-Object -Sum).Sum
+        
+        $points = $maxPoints
+        For($i = 1; $i -le $this.AceCount() -and $points -gt 21 ; $i++)
+        { $points -= 10 }
+        
+        return $points 
+    }
+}
+
+
+$myCard = [Card]::new("K", "H")
 
 Write-Output [string]$myCard
 
 $myCard.points()
+
+
+Write-Output "Hand class now"
+$myHand = 
+@(
+    [Card]::new("2", "H")
+    , [Card]::new("J", "S")
+    , [Card]::new("A", "D")
+)
+$myHand2 = [Hand]::new($myHand)
+
+Write-Output $myHand2.ToString()
+
+Write-Output "Which is" $myHand2.Points() "points"
+Write-Output "With " $myHand2.AceCount() "aces"
