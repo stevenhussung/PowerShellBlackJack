17f805e959f2275f438398e4ec40a90a922ddf23
Steve <steven.hussung@gmail.com>
2024-05-16 15:52:07 -0400

    Create Card.ps1 with Card class

---
+++ Card.ps1
@@ -0,0 +1,31 @@
+class Card {
+  [string] $Suit
+  [string] $Rank
+  
+  Card() {$this.Init('2', 'H')}
+  
+  Card([string]$Rank, [string]$Suit)
+  {
+    $this.Init($Rank, $Suit)
+  }
+  
+  [void] Init([string]$Rank, [string]$Suit)
+  {
+    $this.Rank = $Rank
+    $this.Suit = $Suit
+  }
+  
+  [string] ToString() 
+  {
+    return "$($this.Rank) of $($this.Suit)"
+  }
+  [int] Points()
+  {
+    if ($this.Rank -in @("J", "Q", "K")) 
+    {return 10} 
+    elseif ($this.Rank -eq "A") 
+    {return 11} 
+    else 
+    {return [int]$this.Rank}
+  }
+}
